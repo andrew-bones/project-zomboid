3c11f054cc765d962be0b3be8d60f578a6f3608f
andrew-bones <sebe.on.valt@gmail.com>
2022-01-01 17:07:28 +0100

    add chown after changing linuxgsm uid/gid

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,4 +27,12 @@ if [[ $(stat -c "%u" /server-files) != $UID ]]; then
   chown -R ${runAsUser}:${runAsGroup} /server-files
 fi
 
+if [[ $(stat -c "%u" /home/linuxgsm/Zomboid) != $UID ]]; then
+  chown -R ${runAsUser}:${runAsGroup} /home/linuxgsm/Zomboid
+fi
+
+if [[ $(stat -c "%u" /home/linuxgsm/serverfiles) != $UID ]]; then
+  chown -R ${runAsUser}:${runAsGroup} /home/linuxgsm/serverfiles
+fi
+
 exec gosu ${runAsUser}:${runAsGroup} /entrypoint.sh "$@"
